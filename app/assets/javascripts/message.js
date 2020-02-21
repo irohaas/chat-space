e74e7bb1f050b832b509e88ce139105953592280
irohaas <ginirohas@gmail.com>
2020-02-21 20:57:08 +0900

    スクロール実装

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -54,8 +54,10 @@ $(function(){
   })
    .done(function(data){
      var html = buildHTML(data);
-     $('.messages').append(html);      
+     $('.messages').append(html);
+     $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight});      
      $('form')[0].reset();
+     
    })
 })
 });
